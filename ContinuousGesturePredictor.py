a48f103020e74ef4402262317ee19224984e674c
aryamansriram <aryaman.sriram@gmail.com>
2020-01-07 00:28:21 +0530

    Dummy Image Subscriber Added for reference

--- ContinuousGesturePredictor.py
+++ ContinuousGesturePredictor.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
 import tensorflow as tf
 import tflearn
 from tflearn.layers.conv import conv_2d,max_pool_2d
@@ -55,65 +58,68 @@ def segment(image, threshold=25):
 
 def main():
     # initialize weight for running average
-    aWeight = 0.5
-
-    # get the reference to the webcam
-    camera = cv2.VideoCapture(0)
-
-    # region of interest (ROI) coordinates
-    top, right, bottom, left = 10, 350, 225, 590
-
-    # initialize num of frames
-    num_frames = 0
-    start_recording = False
-
-    # keep looping, until interrupted
-    while(True):
-        # get the current frame
-        (grabbed, frame) = camera.read()
-
-        # resize the frame
-        frame = imutils.resize(frame, width = 700)
-
-        # flip the frame so that it is not the mirror view
-        frame = cv2.flip(frame, 1)
-
-        # clone the frame
-        clone = frame.copy()
-
-        # get the height and width of the frame
-        (height, width) = frame.shape[:2]
-
-        # get the ROI
-        roi = frame[top:bottom, right:left]
-
-        # convert the roi to grayscale and blur it
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
-
-        # to get the background, keep looking till a threshold is reached
-        # so that our running average model gets calibrated
-        if num_frames < 30:
-            run_avg(gray, aWeight)
-        else:
-            # segment the hand region
-            hand = segment(gray)
-
-            # check whether hand region is segmented
-            if hand is not None:
-                # if yes, unpack the thresholded image and
-                # segmented region
-                (thresholded, segmented) = hand
-
-                # draw the segmented region and display the frame
-                cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
-                if start_recording:
-                    cv2.imwrite('Temp.png', thresholded)
-                    resizeImage('Temp.png')
-                    predictedClass, confidence = getPredictedClass()
-                    print((predictedClass,confidence))
-                    showStatistics(predictedClass, confidence)
-                cv2.imshow("Thesholded", thresholded)
+    
+        
+        aWeight = 0.5
+    
+        # get the reference to the webcam
+        camera = cv2.VideoCapture(0)
+    
+        # region of interest (ROI) coordinates
+        top, right, bottom, left = 10, 350, 225, 590
+    
+        # initialize num of frames
+        num_frames = 0
+        start_recording = False
+    
+        # keep looping, until interrupted
+        while(True):
+            # get the current frame
+            (grabbed, frame) = camera.read()
+    
+            # resize the frame
+            frame = imutils.resize(frame, width = 700)
+    
+            # flip the frame so that it is not the mirror view
+            frame = cv2.flip(frame, 1)
+    
+            # clone the frame
+            clone = frame.copy()
+    
+            # get the height and width of the frame
+            (height, width) = frame.shape[:2]
+    
+            # get the ROI
+            roi = frame[top:bottom, right:left]
+    
+            # convert the roi to grayscale and blur it
+            gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    
+            # to get the background, keep looking till a threshold is reached
+            # so that our running average model gets calibrated
+            if num_frames < 30:
+                run_avg(gray, aWeight)
+            else:
+                # segment the hand region
+                hand = segment(gray)
+    
+                # check whether hand region is segmented
+                if hand is not None:
+                    # if yes, unpack the thresholded image and
+                    # segmented region
+                    (thresholded, segmented) = hand
+    
+                    # draw the segmented region and display the frame
+                    cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+                    if start_recording:
+                        cv2.imwrite('Temp.png', thresholded)
+                        resizeImage('Temp.png')
+                        predictedClass, confidence = getPredictedClass()
+                        
+                        print((predictedClass,confidence))
+                        showStatistics(predictedClass, confidence)
+                    cv2.imshow("Thesholded", thresholded)
 
         # draw the segmented hand
         cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
